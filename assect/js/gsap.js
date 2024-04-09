c0f8a8dfcaf2e9b7e9a3d44ab8d885e4f9741979
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-04-09 12:44:12 -0500

    education sec

--- assect/js/gsap.js
+++ assect/js/gsap.js
@@ -1,12 +1,26 @@
-// nav bar start
+// // nav bar start
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.create({
     // markers: true,
     start: "1% top",
-    end:"bottom top",
+    end: "bottom top",
     trigger: "body",
     toggleClass: { targets: "header", className: "base-active" }
 });
 
-// nav bar end
+// // nav bar end
+
+gsap.to(".banner-head", {
+    // markers: true,
+    duration: 1, 
+    y: "-50px",
+    // opacity: 0,
+    scrollTrigger: {
+        start: "top 50%",
+        end: "bottom 50%", 
+        toggleActions: "play none none none" 
+    }
+});
+
+// projects page - end
--- assect/js/main.js
+++ assect/js/main.js
@@ -86,3 +86,19 @@ const swiper = new Swiper('.project-swiper', {
 
 // project carousel - end
 
+
+// education carousel start
+var swiperEdu = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    // loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+// education carousel end
